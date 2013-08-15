063e61b325539d536158e9630434afb8a4cf2fc7
mdsumner <mdsumner@gmail.com>
2013-08-15 21:02:08 +1000

    first non-fail run of curve.model

--- curve.model/run.R
+++ curve.model/run.R
@@ -1,8 +1,16 @@
 library(SGAT)
 
-source("C:\\Users\\michae_sum\\Documents\\GitHub\\SGAT\\R\\Curve.R")
+if ( setInternet2(NA)) {
+    source("C:\\Users\\michae_sum\\Documents\\GitHub\\SGAT\\R\\Curve.R")
+    load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\readyRun.Rdata")
+    load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\old\\metropolisMarch06\\x.Rdata")
+} else {
+    source("C:\\Users\\mdsumner\\Documents\\GitHub\\SGAT\\R\\Curve.R")
+    load("E:\\backup\\Elements\\Azimuth\\DATA\\mcmcGeo\\b362_99\\readyRun.Rdata")
+load("E:\\backup\\Elements\\Azimuth\\DATA\\mcmcGeo\\b362_99\\old\\metropolisMarch06\\x.Rdata")
+}
 
-load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\readyRun.Rdata")
+
 ## don't ask
 body(calib) <- parse(text = ".approxfun(x, y, v, method, yleft, yright, f)")
 
@@ -38,11 +46,11 @@ log.prior <- function(p)  {
   ifelse(f|is.na(f),0,-1000)
 }
 
- load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\old\\metropolisMarch06\\x.Rdata")
+
 
 x0 <- x[,1:2]
 
-fixedx <- c(1, nrow(x0))
+fixedx <- seq_len(nrow(x0)) %in% c(1, nrow(x0))
 
 z0 <-  (x0[-nrow(x0),1:2] + x0[-1,1:2])/2
 model <- curve.model(d1$gmt, d1$light, d1$segment, calib,
@@ -56,13 +64,14 @@ x.proposal <- mvnorm(S=diag(c(0.005,0.005)),n=nrow(x0))
 z.proposal <- mvnorm(S=diag(c(0.005,0.005)),n=nrow(x0)-1)
 
 fit <- estelle.metropolis(model,x.proposal,z.proposal,iters=100,thin=20)
-all(fixedx | model$residuals(chain.last(fit$x)) > 0)
+
+## testthat
+## fixedx[i/n] is really a logical vector of length n, no NAs (and friends)
+## dt is all valid, no funky
+## all arguments are right length, sign and mode
+## calib returns valid values that are increasing over -10:10
+
+
 all(fixedx | is.sea(chain.last(fit$x)))
 all(is.sea(chain.last(fit$z)))
-@
-Once a set of initial locations that meet all the model constraints
-is found, these locations are used as initial points for the Log
-Normal model
-<<>>=
-x0 <- chain.last(fit$x)
-z0 <- chain.last(fit$z)
+
