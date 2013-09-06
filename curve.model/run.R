6626f6f53a5b41c40ff7f1dd42310f6bd15b1215
Michael Sumner <michael.sumner@aad.gov.au>
2013-09-06 17:13:00 +1000

    this is running

--- curve.model/run.R
+++ curve.model/run.R
@@ -1,11 +1,11 @@
 library(SGAT)
 
 if ( setInternet2(NA)) {
-    source("C:\\Users\\michae_sum\\Documents\\GitHub\\SGAT\\R\\SGAT.R")
+    ##source("C:\\Users\\michae_sum\\Documents\\GitHub\\SGAT\\R\\SGAT.R")
     load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\readyRun.Rdata")
     load("D:\\data\\TAGDATA\\mcmcGeo\\b362_99\\old\\metropolisMarch06\\x.Rdata")
 } else {
-    source("C:\\Users\\mdsumner\\Documents\\GitHub\\SGAT\\R\\SGAT.R")
+    ##source("C:\\Users\\mdsumner\\Documents\\GitHub\\SGAT\\R\\SGAT.R")
     load("E:\\backup\\Elements\\Azimuth\\DATA\\mcmcGeo\\b362_99\\readyRun.Rdata")
 load("E:\\backup\\Elements\\Azimuth\\DATA\\mcmcGeo\\b362_99\\old\\metropolisMarch06\\x.Rdata")
 }
@@ -20,39 +20,14 @@ mkOldNew <- function(calibfun) {
 
 newcalib <- mkOldNew(calib)
 
+##Ellieb362_99 <- d1[,c(2, 3, 6)]
+##calibdata <- data.frame(zenith = seq(100, 80, by = 0.2), light = newcalib(seq(100, 80, by = 0.2)))
+
 
 d1 <- d[!is.na(d$segment), ]
 d1 <- d1[d1$depth < 20, ]
 d1 <- d1[!is.na(d1$light), ]
 d1$segment <- unclass(factor(d1$segment))
-
-library(raster)
-library(maptools)
-data(wrld_simpl)
-land.mask <- function(xlim,ylim,n=4,land=TRUE) {
-  r <- raster(nrows=n*diff(ylim),ncols=n*diff(xlim),
-              xmn=xlim[1],xmx=xlim[2],
-              ymn=ylim[1],ymx=ylim[2],
-              crs=proj4string(wrld_simpl))
-  r <- rasterize(wrld_simpl,r)
-  r <- as.matrix(is.na(r))[nrow(r):1,]
-  if(land) r <- !r
-  xbin <- seq(xlim[1],xlim[2],length=ncol(r)+1)
-  ybin <- seq(ylim[1],ylim[2],length=nrow(r)+1)
-
-  function(p) {
-    r[cbind(.bincode(p[,2],ybin),.bincode(p[,1],xbin))]
-  }
-}
-
-is.sea <- land.mask(xlim = c(lon.min, lon.max),
-                    ylim= c(lat.min, lat.max),
-                    n = 4,land = FALSE)
-
-log.prior <- function(p)  {
-  f <- is.sea(p)
-  ifelse(f|is.na(f),0,-1000)
-}
 
 
 
@@ -62,11 +37,8 @@ fixedx <- seq_len(nrow(x0)) %in% c(1, nrow(x0))
 
 z0 <-  (x0[-nrow(x0),1:2] + x0[-1,1:2])/2
 model <- curve.model(d1$gmt, d1$light, d1$segment, newcalib,
-
-                     alpha = c(7, 10), beta = c(150, 80),
-                     logp.x = log.prior,
-                     logp.z = log.prior,
-                   x0 = x0,z0 = z0,
+                     alpha = c(7, 10), beta = c(0.5, 0.08),
+                     x0 = x0,z0 = z0,
                      fixedx = fixedx)
 
 ## find starting points
@@ -81,12 +53,9 @@ for (i in seq_along(grid$x)) {
 
 x0 <- cbind(as.matrix(expand.grid(grid$x, grid$y)[apply(grid$z, 3, which.max), ]), 0)
 
-
-
-
-
-x.proposal <- mvnorm(S=diag(c(0.05,0.05, 0.5)),n=nrow(x0))
-z.proposal <- mvnorm(S=diag(c(0.05,0.05)),n=nrow(x0)-1)
+x.proposal <- mvnorm(S=diag(c(0.005,0.005, 0.05)),n=nrow(x0))
+z.proposal <- mvnorm(S=diag(c(0.005,0.005)),n=nrow(x0)-1)
+##fit <- stella.metropolis(model,x.proposal,iters=100,thin=20, x0 = x0)
 
 fit <- estelle.metropolis(model,x.proposal,z.proposal,iters=100,thin=20, x0 = x0, z0 = (x0[-nrow(x0),1:2]+ x0[-1,1:2])/2)
 
@@ -102,6 +71,16 @@ all(is.sea(chain.last(fit$z)))
 
 x.proposal <- mvnorm(chain.cov(fit$x),s=0.3, tol = 1e-06)
 z.proposal <- mvnorm(chain.cov(fit$z),s=0.3)
+
+
+##fit <- stella.metropolis(model,x.proposal,
+##                          x0=chain.last(fit$x),
+##                          iters=300,thin=20)
+
+
+
+
+
 fit <- estelle.metropolis(model,x.proposal,z.proposal,
                           x0=chain.last(fit$x),z0=chain.last(fit$z),
                           iters=300,thin=20)
